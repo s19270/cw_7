6c5bb2cfa390dc2f4bbd6dce411cc989fef026e5
s19270 <61821772+s19270@users.noreply.github.com>
2020-04-25 18:39:31 +0200

    commit

--- cw_3/cw_3/Controllers/EnrollmentsController.cs
+++ cw_3/cw_3/Controllers/EnrollmentsController.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using cw_3.Login;
 using cw_3.Models;
 using cw_3.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace cw_3.Controllers
 {
@@ -18,6 +24,11 @@ namespace cw_3.Controllers
     {
         public readonly string conString = "Data Source=db-mssql;Initial Catalog=s19270;Integrated Security=True";
         IStudentDbService service = new SqlServerDbService();
+        public IConfiguration Configuration { get; set; }
+        public EnrollmentsController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         [HttpPost]
         public IActionResult CreateStudent(NewStudent student)
         {
@@ -28,6 +39,36 @@ namespace cw_3.Controllers
         {
             return Ok((studies + ", " + semester));
             return Ok(service.PromoteStudents(studies, semester));
+        }
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public IActionResult Login(LoginRequest login)
+        {
+            var claims = new[]
+{
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Name, "doman"),
+                new Claim(ClaimTypes.Role, "employee"),
+                new Claim(ClaimTypes.Role, "student")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SecretKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken
+            (
+                issuer: "Gakko",
+                audience: "Students",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(10),
+                signingCredentials: creds
+            );
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                refreshToken = Guid.NewGuid()
+            });
         }
     }
 }
---
+++ cw_3/cw_3/Login/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw_3.Login
+{
+    public class LoginRequest
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+}
